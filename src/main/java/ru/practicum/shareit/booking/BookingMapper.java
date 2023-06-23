f7fcb0f66e9d6f927de9b28266bd3bed17ac0227
gray-alt <gray-alt@yandex.ru>
2023-06-23 11:41:59 +0300

    refactor: заменить проверку на null в мапперах на optional

--- src/main/java/ru/practicum/shareit/booking/BookingMapper.java
+++ src/main/java/ru/practicum/shareit/booking/BookingMapper.java
@@ -1,6 +1,10 @@
 package ru.practicum.shareit.booking;
 
 import ru.practicum.shareit.booking.dto.BookingDto;
+import ru.practicum.shareit.item.model.Item;
+import ru.practicum.shareit.user.model.User;
+
+import java.util.Optional;
 
 public class BookingMapper {
     public BookingDto toBookingDto(Booking booking) {
@@ -8,8 +12,8 @@ public class BookingMapper {
                 .id(booking.getId())
                 .start(booking.getStart())
                 .end(booking.getEnd())
-                .itemId(booking.getItem() != null ? booking.getItem().getId() : null)
-                .bookerId(booking.getBooker() != null ? booking.getBooker().getId() : null)
+                .itemId(Optional.ofNullable(booking.getItem()).map(Item::getId).orElse(null))
+                .bookerId(Optional.ofNullable(booking.getBooker()).map(User::getId).orElse(null))
                 .status(booking.getStatus())
                 .build();
     }
